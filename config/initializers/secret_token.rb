25867b5ec8e1ed33ea14b86b16954a438e48a9d4
Sabar Dasgupta <sabar@orchive.com>
2014-02-08 12:12:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BtcExchange::Application.config.secret_key_base = '6f09258bbbd4354a404bddd361cb94035bee1fd6d8655fc1be17145a45f45a4541525928ec2e4b18d513eb09ab678a84df6481076e91089e9972c7dbad6e5157'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BtcExchange::Application.config.session_store :cookie_store, key: '_btc_exchange_session'
